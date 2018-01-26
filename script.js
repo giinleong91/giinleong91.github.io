c93927cd20113e21afa2b54772cc8c54198d8f11
giinleong91 <35290374+giinleong91@users.noreply.github.com>
2018-01-27 03:04:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+  var dropdown = document.querySelector("a.icon");
+dropdown.onclick =  function() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
